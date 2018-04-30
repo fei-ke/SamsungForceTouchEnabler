35a28bd16cdafdcc5fe79f27ca8be41a2237a778
fei-ke <jinyang656@qq.com>
2018-04-30 18:42:48 +0800

    navi bar height configuration

--- app/src/main/java/com/fei_ke/forcetouchenabler/MainActivity.java
+++ app/src/main/java/com/fei_ke/forcetouchenabler/MainActivity.java
@@ -1,12 +1,76 @@
 package com.fei_ke.forcetouchenabler;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+
+        if (checkCallingOrSelfPermission("android.permission.WRITE_SECURE_SETTINGS") != PackageManager.PERMISSION_GRANTED)
+            try {
+                Runtime.getRuntime().exec("su -c pm grant " + BuildConfig.APPLICATION_ID + " android.permission.WRITE_SECURE_SETTINGS")
+                        .waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        Switch switchSwitch = findViewById(R.id.switchCustomHeight);
+        final TextView textViewHeight = findViewById(R.id.textViewHeight);
+        final SeekBar seekBarHeight = findViewById(R.id.seekBarHeight);
+
+        final ContentResolver resolver = getContentResolver();
+
+        final int isCustom = Settings.Global.getInt(resolver, BuildConfig.APPLICATION_ID + ".is_custom_navi_bar_height", 0);
+        switchSwitch.setChecked(isCustom == 1);
+
+        switchSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Settings.Global.putInt(resolver, BuildConfig.APPLICATION_ID + ".is_custom_navi_bar_height", isChecked ? 1 : 0);
+
+                textViewHeight.setEnabled(isChecked);
+                seekBarHeight.setEnabled(isChecked);
+            }
+        });
+
+        seekBarHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewHeight.setText(String.valueOf(progress));
+                Settings.Global.putInt(resolver, BuildConfig.APPLICATION_ID + ".custom_navi_bar_height", progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        int height = Settings.Global.getInt(resolver, BuildConfig.APPLICATION_ID + ".custom_navi_bar_height", 45);
+        seekBarHeight.setProgress(height);
     }
 }
--- app/src/main/java/com/fei_ke/forcetouchenabler/XposedMod.java
+++ app/src/main/java/com/fei_ke/forcetouchenabler/XposedMod.java
@@ -1,10 +1,12 @@
 package com.fei_ke.forcetouchenabler;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 
 import java.lang.reflect.Method;
 
@@ -17,20 +19,46 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.findMethodBestMatch;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
+import static de.robv.android.xposed.XposedHelpers.setBooleanField;
 
 public class XposedMod implements IXposedHookLoadPackage {
+    private static final String TAG = "XposedMod";
+
     private static final int EVENT_ID = 2;
 
-    private static final int MSG_TAP        = 1;
+    private static final int MSG_TAP = 1;
     private static final int MSG_LONG_PRESS = 2;
-
 
     @Override
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("android")) return;
+        if (lpparam.packageName.equals("android")) {
+            hookNaviBarForceTouchManager(lpparam);
+            hookPhoneWindowManager(lpparam);
+        }
+    }
 
+    private void hookPhoneWindowManager(LoadPackageParam lpparam) {
+        Class<?> clazz = findClass("com.android.server.policy.PhoneWindowManager", lpparam.classLoader);
+        findAndHookMethod(clazz, "getNavigationBarHeight", int.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Context context = (Context) getObjectField(param.thisObject, "mContext");
+                ContentResolver resolver = context.getContentResolver();
+
+                int isCustom = Settings.Global.getInt(resolver, BuildConfig.APPLICATION_ID + ".is_custom_navi_bar_height", 0);
+                if (isCustom == 1) {
+                    int heightDp = Settings.Global.getInt(resolver, BuildConfig.APPLICATION_ID + ".custom_navi_bar_height", 160);
+                    int heightPx = (int) (heightDp * context.getResources().getDisplayMetrics().density + 0.5f);
+                    param.setResult(heightPx);
+                }
+            }
+        });
+    }
+
+    private void hookNaviBarForceTouchManager(LoadPackageParam lpparam) {
         final Class<?> clazz = findClass("com.android.server.policy.NaviBarForceTouchManager", lpparam.classLoader);
         final Method forceClickImmersive = findMethodBestMatch(clazz, "forceClickImmersive", int.class);
+        final Method setHomeConsumed = findMethodBestMatch(clazz, "setHomeConsumed");
 
         @SuppressLint("HandlerLeak") final Handler handler = new Handler() {
             @Override
@@ -65,15 +93,17 @@ public class XposedMod implements IXposedHookLoadPackage {
                     handler.removeMessages(MSG_TAP);
                     //double tap
 
+                    setHomeConsumed.invoke(param.thisObject);
+
                     Context context = (Context) getObjectField(param.thisObject, "mContext");
                     Intent cameraIntent = context.getPackageManager().getLaunchIntentForPackage("com.sec.android.app.camera");
                     cameraIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
                     context.startActivity(cameraIntent);
-
-                    param.setResult(null);
                 } else {
                     handler.sendEmptyMessageDelayed(MSG_TAP, 600);
                 }
+
+                setBooleanField(param.thisObject, "mNaviBarVisible", false);
             }
         });
     }
